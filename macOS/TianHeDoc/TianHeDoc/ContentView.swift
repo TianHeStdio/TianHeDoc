a21453b7da257ca78e94404cca13655dad29574d
AdceStdio <btmwangdaika@gmail.com>
2024-04-04 13:02:46 +0800

    Get ready to coding

---
+++ macOS/TianHeDoc/TianHeDoc/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TianHeDoc
+//
+//  Created by mac on 2024/4/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ macOS/TianHeDoc/TianHeDoc/TianHeDocApp.swift
@@ -0,0 +1,17 @@
+//
+//  TianHeDocApp.swift
+//  TianHeDoc
+//
+//  Created by mac on 2024/4/4.
+//
+
+import SwiftUI
+
+@main
+struct TianHeDocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
