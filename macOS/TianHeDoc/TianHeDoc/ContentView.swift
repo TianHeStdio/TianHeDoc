db69b839866847df9124440077a79660496b2d11
AdceStdio <btmwangdaika@gmail.com>
2024-04-04 15:30:13 +0800

    Get ready to coding

--- macOS/TianHeDoc/TianHeDoc/ContentView.swift
+++ macOS/TianHeDoc/TianHeDoc/ContentView.swift
@@ -9,10 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+        VStack { 
             Text("Hello, world!")
         }
         .padding()
